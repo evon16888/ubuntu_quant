4727953e79b03522f33d4ad5c00d9e774a32b281
evon16888 <evon16888@gmail.com>
2020-04-11 14:25:58 +0800

    Update bin.sh

--- bin.sh
+++ bin.sh
@@ -9,17 +9,17 @@ echo "正在安装pip3"
 sudo apt-get install python3-pip
 
 echo "正在安装pandas"
-pip3 install pandas
+sudo pip3 install pandas
 
 echo "正在安装ccxt"
-pip3 install ccxt
+sudo pip3 install ccxt
 
 echo "正在安装pymysql"
-pip3 install pymysql
+sudo pip3 install pymysql
 
 echo "正在安装sqlalchemy"
 
-pip3 install sqlalchemy
+sudo pip3 install sqlalchemy
 
 echo "安装编译工具"
 sudo apt install build-essential wget -y
