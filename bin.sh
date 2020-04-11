d31e635b90db02e1edd43cd1d97e05ed1fa137e1
evon16888 <evon16888@gmail.com>
2020-04-11 14:15:16 +0800

    Update bin.sh

--- bin.sh
+++ bin.sh
@@ -21,5 +21,18 @@ echo "正在安装sqlalchemy"
 
 pip3 install sqlalchemy
 
+echo "安装编译工具"
+sudo apt install build-essential wget -y
+
+echo "下载源码并解包"
+wget https://artiya4u.keybase.pub/TA-lib/ta-lib-0.4.0-src.tar.gz
+tar -xvf ta-lib-0.4.0-src.tar.gz
+cd ta-lib/
+echo "开始编译安装TA-Lib"
+./configure --prefix=/usr
+make
+sudo make install
+sudo pip3 install TA-Lib
+
 echo "正在安装宝塔"
 wget -O install.sh http://download.bt.cn/install/install-ubuntu_6.0.sh && sudo bash install.sh
