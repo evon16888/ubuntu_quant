b0c5ab6a54f152b64d9799139838674bb19a6bf9
evon16888 <evon16888@gmail.com>
2020-05-01 11:17:17 +0800

    Create talib_install.sh

---
+++ talib_install.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
+export PATH
+
+echo "正在系统更新..."
+sudo apt-get update
+sudo apt install build-essential wget -y
+wget https://artiya4u.keybase.pub/TA-lib/ta-lib-0.4.0-src.tar.gz
+tar -xvf ta-lib-0.4.0-src.tar.gz
+cd ta-lib/
+./configure --prefix=/usr
+make
+sudo make install
+sudo pip3 install TA-Lib
+echo "安装完毕"
