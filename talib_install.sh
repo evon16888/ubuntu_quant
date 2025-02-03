8cf6e85702064ee45e73057cc66bea3ad6199efc
evon16888 <evon16888@gmail.com>
2025-02-03 20:27:28 +0800

    Update talib_install.sh

--- talib_install.sh
+++ talib_install.sh
@@ -5,7 +5,7 @@ export PATH
 echo "正在系统更新..."
 sudo apt-get update
 sudo apt install build-essential wget -y
-wget https://artiya4u.keybase.pub/TA-lib/ta-lib-0.4.0-src.tar.gz
+wget http://prdownloads.sourceforge.net/ta-lib/ta-lib-0.4.0-src.tar.gz
 tar -xvf ta-lib-0.4.0-src.tar.gz
 cd ta-lib/
 ./configure --prefix=/usr
